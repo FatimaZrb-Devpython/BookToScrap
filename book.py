74c44d2ca6ea3958ed0725811c7e7d9b36db2f1b
zarab fatima <zarab.fatima@gmail.com>
2023-10-25 21:05:18 +0200

    modification de la fonction pour collecter les donners d'un seul livre

--- book.py
+++ book.py
@@ -5,55 +5,63 @@ from bs4 import BeautifulSoup
 
 
 
-def collect(url) : 
+def collect_one_book(link) : 
     
-    page = requests.get(url)
+    page = requests.get(link)
     soup = BeautifulSoup(page.content, 'html.parser')
 
+#Extraction du titre dans le DOM
     titles = soup.find("h1")
     title = []
     title.append(titles.string)
-    
-    ps = soup.find_all('p')
-    p = []
-    for p in ps : 
-        description = ps[3]
-        p.append(description.string)
 
+#Extraction de toutes les balises p dans le DOM afin d'obtenir la balise p arrivant en 3eme position pour récupérer la description
+   
+    descriptions = soup.find_all('p')[3]
+    description = []
+    description.append(descriptions.string)
+
+#Extraction de l'url de l'image du livre
     images = soup.find("div", id='product_gallery')
-    image = [] 
-    for image in images : 
-        img = image.find("img")
-        url = img['src']
-        image.append(url.string)
+    image = []
+    img = images.find("img")
+    src = img.get('src')
+    image.append(src.strip())
 
-    tds = soup.find_all('td')
-    td = []
-    for td in tds : 
-            upc = tds[0]
-            td.append(upc.string)
-            
-            price_excl_tax=tds[2]
-            td.append(price_excl_tax)
-            
-            price_incl_tax=tds[3]
-            td.append(price_incl_tax.string)
-            
-            available=tds[5]
-            td.append(available.string)
-            
-            category=tds[1]
-            td.append(category.string)
-            
-            review=tds[6]
-            td.append(review.string)
-        
-            # return title,description,upc,price_excl_tax,price_incl_tax,available,review,url
+    
+# Extraction de la balise table afin de récupérer les balises td qui correspondent à chaque éléments nécessaire
 
-title,description,upc,price_excl_tax,price_incl_tax,available,review,url,image = collect("http://books.toscrape.com/catalogue/shakespeares-sonnets_989/index.html")
+    table = soup.find("table",class_='table table-striped')
+    tds = table.find_all('td')
 
+    list = []
+    
+    upc = tds[0]# Code UPC du livre 
+    list.append(('upc',upc))
+            
+    category = tds[1]  # De quel catégorie est le produit
+    list.append(('category',category))
+            
+    price_excl_tax = tds[2] # Prix hors taxe du livre 
+    list.append(('price_excl_tax',price_excl_tax))
+            
+    price_incl_tax = tds[3] # Prix avec taxe du livre
+    list.append(('price_incl_tax',price_incl_tax))
+            
+    available = tds[5] # Quantité restant du livre
+    list.append(('available',available))
+            
+    review = tds[6] # Nombre d'étoile du livre
+    list.append(('review',review))
+    
+            
+    return title,description,image,upc,category,price_excl_tax,price_incl_tax,available,review         
+
+link = collect_one_book("http://books.toscrape.com/catalogue/shakespeares-sonnets_989/index.html")
+
+    
 # def write () :
-# # Créer une liste pour les en-têtes
+# # Créer une tde pour les en-têtes
 #     en_tete = ["title","description","upc","price_excl_tax","price_incl_tax","available","review","url"]
 
 # # Créer un nouveau fichier pour écrire dans le fichier appelé « book.csv »
@@ -61,7 +69,7 @@ title,description,upc,price_excl_tax,price_incl_tax,available,review,url,image =
 #         # Créer un objet writer (écriture) avec ce fichier
 #         writer = csv.writer(csv_file, delimiter=',')
 #         writer.writerow(en_tete)
-#         # Parcourir les éléments- zip permet d'itérer sur deux listes ou plus à la fois
+#         # Parcourir les éléments- zip permet d'itérer sur deux tdes ou plus à la fois
 #         for title,description,upc,price_excl_tax,price_incl_tax,available,review,url in zip(collect()):
 #             # Créer une nouvelle ligne pour chaque éléments à ce moment de la boucle
 #             ligne = [title,description,upc,price_excl_tax,price_incl_tax,available,review,url] 
