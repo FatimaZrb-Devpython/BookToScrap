f35d0113c35fe86b06624f71b4f8e809ec9e3bb5
zarab fatima <zarab.fatima@gmail.com>
2023-12-16 16:32:23 +0100

    fonction terminé

--- get_all_categories.py
+++ get_all_categories.py
@@ -1,36 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
-# import csv
+import csv
 
 def get_all_categories (url):
     
-    
     categories = []
 
-    response = requests.get(url)
+    response = requests.get(url, timeout=(10, 30))
     soup = BeautifulSoup(response.content, 'html.parser')
         
     list = soup.find("ul", class_='nav nav-list').find_all('li')
     for a in list :
         a =  a.find('a')
         link = a.get('href')
-        categories.append('https://books.toscrape.com/' + link)
+        url_book = ('https://books.toscrape.com/' + link)
+        split = url_book.split('/')
+        title_category = split[6]
+        categories.append(('title : ',title_category, 'url :',url_book))
         
     return categories 
 
 
+def save_categories (categories) : 
 
-# Créer une liste pour les en-têtes
-# en_tete = ["home"]
+    # Créer une liste pour les en-têtes
+    en_tete = ["category"]
 
-# # Créer un nouveau fichier pour écrire dans le fichier appelé « category.csv »
-# with open('all_book.csv', 'w') as csv_file:
-#     # Créer un objet writer (écriture) avec ce fichier
-#     writer = csv.writer(csv_file, delimiter=',')
-#     writer.writerow(en_tete)
-#     # Parcourir les éléments- zip permet d'itérer sur deux listes ou plus à la fois
-#     for link in zip():
-#         # Créer une nouvelle ligne pour chaque éléments à ce moment de la boucle
-#         ligne = [link] 
-#         writer.writerow(ligne)
+    # Créer un nouveau fichier pour écrire dans le fichier appelé « category.csv »
+    with open('all_categories.csv', 'w') as csv_file:
+        # Créer un objet writer (écriture) avec ce fichier
+        writer = csv.writer(csv_file, delimiter=';')
+        writer.writerow(en_tete)
+
+        # Créer une nouvelle ligne pour chaque éléments à ce moment de la boucle
+        writer.writerow(categories) 
+        
         
