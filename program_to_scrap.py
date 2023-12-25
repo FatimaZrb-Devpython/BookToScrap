b593b700d307aa29edc4ff8b99778249ffa945a2
zarab fatima <zarab.fatima@gmail.com>
2023-12-25 22:15:07 +0100

    programme terminé

--- program_to_scrap.py
+++ program_to_scrap.py
@@ -7,22 +7,22 @@ url = 'https://books.toscrape.com/index.html'
 
 # Récupérer toutes les catégories
 categories = get_all_categories(url)
-# save_categories(categories)
+save_categories(categories)
 
 # Parcourir chaque catégorie
 for category in categories:
    # Récupérer les livres pour cette catégorie
     books = get_books_one_category(category)
      
-    # save_books_category(books, category)
+    save_books_category(books, category)
 
-    # # Parcourir chaque livre de la catégorie actuelle
+    # Parcourir chaque livre de la catégorie actuelle
     for book in books:
         # Obtenir les détails du livre
         title,description,picture,category_book,upc,price_excl_tax,price_incl_tax,available,review= get_one_book(book)
         
         # Sauvegarder le livre dans le fichier correspondant à la catégorie
-        # save_book(title,description,picture,category_book,upc,price_excl_tax,price_incl_tax,available,review,category)
+        save_book(title,description,picture,category_book,upc,price_excl_tax,price_incl_tax,available,review,category)
         get_picture(title,picture,category)
 
 
